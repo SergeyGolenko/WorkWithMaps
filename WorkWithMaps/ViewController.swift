b386a4dc60ea9253fedd564b7072858817287dc1
SergeyGolenko <1989sergiotay@gmail.com>
2020-12-24 14:26:55 +0200

    done

--- WorkWithMaps/ViewController.swift
+++ WorkWithMaps/ViewController.swift
@@ -6,12 +6,32 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController ,MKMapViewDelegate{
+    
+    @IBOutlet weak var mapWhatShowKharkov: MKMapView!
+    
+    //load coordinate for Kharkov
+    func loadCoordinateKharkov(){
+        let longitudeCoordinate = CLLocationDegrees(36.232845)
+        let latitudeCoordinate = CLLocationDegrees(49.988358)
+        
+        let locationCoordinate2D = CLLocationCoordinate2D(latitude: latitudeCoordinate, longitude: longitudeCoordinate)
+        
+        let locationDistance = CLLocationDistance(10000)
+        
+       
+        let center = MKCoordinateRegion(center: locationCoordinate2D, latitudinalMeters: locationDistance, longitudinalMeters: locationDistance)
+        
+        mapWhatShowKharkov.setRegion(center, animated: true)
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        loadCoordinateKharkov()
     }
 
 
